89a28d610a47fab303d62712a7b73dd760a35fa6
brettonmaybee <brettonmaybee@users.noreply.github.com>
2020-10-20 23:24:36 -0700

    backup commit

--- EmployeeSQL/EmployeesERD.sql
+++ EmployeeSQL/EmployeesERD.sql
@@ -4,61 +4,37 @@
 
 
 CREATE TABLE "department" (
-    "dept_no" PARCHAR(4)   NOT NULL,
-    "dept_name" PARCHAR(30)   NOT NULL
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
 );
 
 CREATE TABLE "dept_emp" (
-    "dept_no" PARCHAR(4)   NOT NULL,
-    "emp_no" INT   NOT NULL
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" varchar(10)   NOT NULL
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" PARCHAR(4)   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
     "emp_no" INT   NOT NULL
 );
 
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
-    "emp_title" PARCHAR(5)   NOT NULL,
+    "emp_title" VARCHAR(20)   NOT NULL,
     "birth_date" date   NOT NULL,
-    "first_name" PARCHAR(15)   NOT NULL,
-    "last_name" PARCHAR(15)   NOT NULL,
-    "sex" PARCHAR(1)   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR()   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
 );
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
+    "salary" INT   NOT NULL
 );
 
 CREATE TABLE "titles" (
-    "title_id" PARCHAR(30)   NOT NULL,
-    "title" PARCHAR(30)   NOT NULL
+    "title_id" VARCHAR(30)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL
 );
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "department" ("dept_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "department" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "salaries" ("emp_no");
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
-REFERENCES "titles" ("title");
 
